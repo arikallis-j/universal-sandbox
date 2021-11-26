e349a7b79245cc31c280ece87fb34a33e98bf2cb
arikallis-j <chernosov.vd@phystech.edu>
2021-11-26 12:16:09 +0300

    ЭТО БАЗА

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello world")
+import numpy as np
+import pygame
+
+from draw import *
+from calc import *
+from const import *
+from config import *
+
+pygame.init()
+
+clock = pygame.time.Clock() #таймер
+screen.fill(SPACE)
+pygame.display.update()
+
+run = True
+while run:
+    clock.tick(FPS)
+    screen.fill(SPACE)
+    Draw()
+    UpDate()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+
+pygame.quit()
